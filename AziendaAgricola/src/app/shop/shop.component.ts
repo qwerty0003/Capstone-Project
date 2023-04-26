26271713f817c63fa372bbe107cb838482b209d5
Andrea Rossi <vinrossiandrea96@gmail.com>
2023-04-26 11:21:31 +0200

    snackbar in shop

--- AziendaAgricola/src/app/shop/shop.component.ts
+++ AziendaAgricola/src/app/shop/shop.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProdottiServiceService } from '../prodotti-service.service';
 import { Prodotto } from '../prodotti/prodotto';
 import { CarrelloService } from '../carrello.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shop',
@@ -13,7 +15,12 @@ export class ShopComponent implements OnInit {
   loaded = false;
   quantitaSelezionata: number = 1;
 
-  constructor(private prodottiService: ProdottiServiceService, private carrelloService: CarrelloService) {}
+  constructor(
+    private prodottiService: ProdottiServiceService,
+    private carrelloService: CarrelloService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.getProdotti();
@@ -23,7 +30,7 @@ export class ShopComponent implements OnInit {
     this.prodottiService.getProducts().subscribe(
       (data) => {
         this.prodotti = data;
-        this.loaded=true;
+        this.loaded = true;
       },
       (error) => console.error(error)
     );
@@ -31,6 +38,19 @@ export class ShopComponent implements OnInit {
 
   aggiungiAlCarrello(prodotto: Prodotto) {
     this.carrelloService.aggiungiAlCarrello(prodotto, this.quantitaSelezionata);
+
     console.log(this.carrelloService.getCarrello());
+
+    let snackBarRef = this.snackBar.open(
+      'Prodotto aggiunto al carrello.',
+      'Visualizza carrello',
+      {
+        duration: 3000,
+      }
+    );
+
+    snackBarRef.onAction().subscribe(() => {
+      this.router.navigate(['/carrello']);
+    });
   }
 }
